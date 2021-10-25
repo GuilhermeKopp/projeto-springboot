46c9511ba841d4deeddd5ecafed0455e45146cab
GuilhermeKopp <guikopp19@gmail.com>
2021-10-25 16:25:04 -0300

    Registro componentes

---
+++ src/main/java/com/guilhermekopp/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.guilhermekopp.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilhermekopp.course.entities.User;
+import com.guilhermekopp.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
